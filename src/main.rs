729a6930c9a793e7a5291fc377f628a03222e14d
AeronauticBlueberry <aerodrome92@gmail.com>
2023-09-07 10:37:36 -0700

    some structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+struct Scene {
+    tag: String,
+    descs: Vec<Desc>,
+    is_end: bool,
+    options: Vec<Option>
+}
+
+struct Desc {
+    text: String,
+    min_san: i32,
+    san_change: i32
+}
+
+struct Option {
+    to_scene: String,
+    min_san: i32
+}
+
+struct Gamestate {
+    current_scene: Scene,
+    sanity: i32
+}
+
 fn main() {
     println!("Hello, world!");
 }
