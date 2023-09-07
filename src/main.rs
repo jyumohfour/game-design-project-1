981649ad29b60d773dc0bf4ee0a150c0d2a5488d
AeronauticBlueberry <aerodrome92@gmail.com>
2023-09-07 11:45:01 -0700

    Reading from JSON

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
 struct Scene {
     tag: String,
     descs: Vec<Desc>,
-    is_end: bool,
     options: Vec<Option>
 }
 
+#[derive(Deserialize)]
 struct Desc {
     text: String,
-    min_san: i32,
-    san_change: i32
+    min_san: i32
 }
 
+#[derive(Deserialize)]
 struct Option {
     to_scene: String,
-    min_san: i32
+    text: String,
+    text_when_chosen: String,
+    min_san: i32,
+    max_san: i32,
+    san_change: i32
 }
 
 struct Gamestate {
@@ -21,6 +32,23 @@ struct Gamestate {
     sanity: i32
 }
 
+fn read_scene_data<P: AsRef<Path>>(path: P) -> Result<Vec<Scene>, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file as an instance of `User`.
+    let u = serde_json::from_reader(reader)?;
+
+    // Return the `User`.
+    Ok(u)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let scenes = read_scene_data("scenes.json").unwrap();
+    
+    // debug - printing room data from the json
+    println!("{}", scenes[0].tag);
+    println!("{}", scenes[0].descs[0].text);
+    println!("{}", scenes[0].options[0].text);
+    println!("{}", scenes[0].options[0].text_when_chosen);
 }
